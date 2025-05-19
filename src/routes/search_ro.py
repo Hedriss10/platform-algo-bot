864a47f2ba35e169de86af4ed187f21c55711bcb
Hedris Pereira <hedris.pereira@eemovel.com>
2025-05-18 23:36:53 -0300

    add: adjust search_ro

--- src/routes/search_ro.py
+++ src/routes/search_ro.py
@@ -5,6 +5,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/search_ro")
+@router.get("/search_ro", tags=["search_ro"])
 async def search_ro():
     return {"message": "Search for RO data"}
